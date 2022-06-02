09bd6c842bcf14df26cc1ea66fd94fba80c34d6d
ar.mmarra@gmail.com <ar.mmarra@gmail.com>
2022-06-02 08:49:33 -0300

    Change script.js and index.html

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,15 +1,19 @@
+//Variables
+
 const menuButton = document.getElementById('button');
 const navMenu = document.getElementById('nav');
-const page = document.getElementById('picture');
+const page = document.getElementById('main');
+
+//Functions
 
 const hideMenu = () => {
     navMenu.style.display = "none";
 }
 
-menuButton.onclick = function click() {
+const showMenu = () => {
     navMenu.style.display = "block";
-};
+}
 
+menuButton.onclick = showMenu;
 navMenu.onmouseleave = hideMenu;
-
 page.onclick = hideMenu;

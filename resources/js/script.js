32dbba6d6dbab2b4bb1217dfe4a18833d4cad0f9
ar.mmarra@gmail.com <ar.mmarra@gmail.com>
2022-06-04 08:28:33 -0300

    Refine some CSS Style. Add some show, hide functionality

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,17 @@
 //Variables
 
+//Menu
 const menuButton = document.getElementById('button');
 const navMenu = document.getElementById('nav');
 const page = document.getElementById('main');
+
+//Descriptions
+const pKelvin = document.getElementById('plusKelvin');
+const kelvinDesc = document.getElementById('kelvin');
+const pWine = document.getElementById('plusWine');
+const wineDesc = document.getElementById('wine');
+const pBroadway = document.getElementById('plusBroadway');
+const broadwayDesc = document.getElementById('broadway');
 
 //Functions
 
@@ -11,9 +20,29 @@ const hideMenu = () => {
 }
 
 const showMenu = () => {
-    navMenu.style.display = "block";
+    // navMenu.style.display = "block";
+    if (navMenu.style.display !== "none") {
+        navMenu.style.display = "none";
+      } else {
+        navMenu.style.display = "block";
+      }
 }
 
+const showDescription = () => {
+    if (kelvinDesc.style.display !== "none") {
+        kelvinDesc.style.display = "none";
+      } else {
+        kelvinDesc.style.display = "block";
+      }
+}
+
+//Listeners
+
+//Menu
 menuButton.onclick = showMenu;
 navMenu.onmouseleave = hideMenu;
 page.onclick = hideMenu;
+
+//Descriptions
+pKelvin.addEventListener('click', showDescription);
+// pKelvin.onclick = showDescription;
